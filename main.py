c1b6a66e98d3bbc9227d6607a47df7b6f6426777
Your Name <you@example.com>
2025-07-09 13:08:42 +0300

    ветвление(выбирает только с node в имени файла) + дописывает название контролируемой метрики в название

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ CONFIG_DIR = "/home/nekto/AWG/projects/Восток-Сервис/alert_rules_gen
 OUTPUT_DIR = ".result"
 TEMPLATE_DIR = "templates"
 
-def load_targets_with_node(config_dir: str):
+#загрузка всех конфигураций
+def load_targets(config_dir: str):
     targets = []
     for file_path in Path(config_dir).glob("*node*.json"):
         with open(file_path, "r", encoding="utf-8") as f:
@@ -18,11 +19,11 @@ def load_targets_with_node(config_dir: str):
                 targets.append((target, file_path.name))
     return targets
 
-def write_alert(output_dir: str, host: str, alert_data: dict):
+def write_alert(output_dir: str, host: str, alert_data: dict, controlled: str):
     # Создаём директорию для вывода, если она ещё не существует
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     # Формируем имя выходного файла на основе имени хоста
-    filename = f"alert_{host}.yml"
+    filename = f"alert_{host}_{controlled}.yml"
     # Полный путь к файлу: директория + имя файла
     output_path = Path(output_dir) / filename
     # Открываем файл на запись в формате UTF-8
@@ -32,14 +33,21 @@ def write_alert(output_dir: str, host: str, alert_data: dict):
     #print(f"✅ Alert written to {output_path}")
 
 def main():
-    targets_with_files = load_targets_with_node(CONFIG_DIR)
+    targets_with_files = load_targets(CONFIG_DIR)
     print(f"Найдено {len(targets_with_files)} целей")  # для отладки
 
     for target, filename in targets_with_files:
-        host = target["labels"]["host"]
-        #print(f"Обрабатываем {host} из {filename}")  # для отладки
-        alert_rule = generate_alert_rule_for_nodes(target, TEMPLATE_DIR)
-        write_alert(OUTPUT_DIR, host, alert_rule)
+        # Проверяем, что 'node' есть в имени файла (без учёта регистра)
+        if "node" in filename.lower():
+            host = target["labels"]["host"]
+            alert_rule = generate_alert_rule_for_nodes(target, TEMPLATE_DIR)
+            controlled = filename.rsplit("_", 1)[-1].split(".", 1)[0]
+            write_alert(OUTPUT_DIR, host, alert_rule, controlled)
+        else:
+            # Можно добавить пропуск или лог, если нужно
+            # print(f"Пропускаем {filename}, не содержит 'node'")
+            pass
+
 
 if __name__ == "__main__":
     main()
